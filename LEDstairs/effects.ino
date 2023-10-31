44c3cfc8e0262049304dc0bd2a56e7eabbba8b7f
saavadas <slava_0629@bk.ru>
2023-11-01 01:24:18 +0500

    Test version for dimas no fade

--- LEDstairs/effects.ino
+++ LEDstairs/effects.ino
@@ -9,12 +9,13 @@ void stepFader(bool dir, bool state) {
   byte mode = state | (dir << 1);
   byte counter = 0;
   while (1) {
-    EVERY_MS(FADR_SPEED) {
+    if (millis() - tmr >= FADR_SPEED) {
       counter++;
+      tmr = millis();
       switch (mode) {
         case 0: staticColor(1, 0, counter); break;
-        case 1: staticColor(1, counter, STEP_AMOUNT); break;
-        case 2: staticColor(-1, STEP_AMOUNT - counter, STEP_AMOUNT); break;
+        case 1: staticColor(-1, counter, STEP_AMOUNT); break;
+        case 2: staticColor(1, STEP_AMOUNT - counter, STEP_AMOUNT); break;
         case 3: staticColor(-1, 0, STEP_AMOUNT - counter); break;
       }
       show();
@@ -35,81 +36,124 @@ void stepFader(bool dir, bool state) {
 #define MIN_SAT 220     // мин. насыщенность
 #define MAX_SAT 255     // макс. насыщенность
 
-
 uint32_t getPixColor(CRGB thisPixel) {
   return (((uint32_t)thisPixel.r << 16) | (thisPixel.g << 8) | thisPixel.b);
 }
 
-// ========= смена цвета общая
-void staticColor(int8_t dir, byte from, byte to) {
-  LEDdata color = mRGB(44, 228, 228); //МЕНЯТЬ ЦВЕТ ТУТ
-  FOR_i(0, STEP_AMOUNT) {
-    if (i < from || i >= to) fillStep(i, mRGB(0, 0, 0));
-    else {
-      uint32_t tmr = millis();
-      while (millis() - tmr < 400) {}
-      fillStep(i, color);
-      //    Serial.println(i);}
+void stepsTurn(int8_t dir, bool mode) {
+  if (dir == 1) {
+    for (int i = 0; i < STEP_AMOUNT; i++) {
+      fillStep(i, color, mode);
+      show();
+    }
+  } else {
+    for (int i = STEP_AMOUNT - 1; i >= 0; i--) {
+      fillStep(i, color, mode);
+      show();
     }
   }
 }
-  /*void staticColor(int8_t dir, byte from, byte to) {
-    LEDdata color = mRGB(44, 228, 228); //МЕНЯТЬ ЦВЕТ ТУТ
-    FOR_i(0, STEP_AMOUNT) {
-      if (i < from || i >= to) fillStep(i, mHSV(0, 255, 0));
-    //    EVERY_MS(300) {
-      else{
-        fillStep(i, color);
-    //    }
-      }
+
+void stepsTurnOff(int8_t dir) {
+  if (dir == 1) {
+    for (int i = 0; i < STEP_AMOUNT; i++) {
+      fillStep(i, mRGB(0, 0, 0), false);
+      show();
     }
-    }*/
-  // ========= залить ступеньку цветом (служебное)
-  void fillStep(int8_t num, LEDdata color) {
-    if (num >= STEP_AMOUNT || num < 0) return;
-    for (int i = steps_start[num]; i < steps_start[num] + steps[num].led_amount; i++) {
+  } else {
+    for (int i = STEP_AMOUNT - 1; i >= 0; i--) {
+      fillStep(i, mRGB(0, 0, 0), false);
+      show();
+    }
+  }
+}
+
+void fadeStep(int num, bool mode) {
+  byte x = 0;
+  Serial.println(curBright);
+  if (mode) {
+    while (x < curBright) {
+//      Serial.println(x);
+      for (int i = steps_start[num]; i < steps_start[num] + steps[num].led_amount; i++) {
+//        strip.fade(i, curBright - x);
+//        setBrightness(curBright);
+      }
+      show();
+      x += 5;
+//      delay(5);
+    }
+  } else {
+    while (curBright - x > 0) {
+//      Serial.println(x);
+      for (int i = steps_start[num]; i < steps_start[num] + steps[num].led_amount; i++) {
+//        strip.fade(i, x);
+//        Serial.print("i ");
+//        Serial.println(i);
+//        Serial.print("x ");
+//        Serial.println(x);
+      }
+      x += 5;
+      show();
+//      delay(50);
+    }
+  }
+}
+
+// ========= смена цвета общая
+
+void staticColor(int8_t dir, byte from, byte to) {
+  for (int i = 0; i < STEP_AMOUNT; i++) {
+    if (i < from || i > to) continue;
+    else {
+      if (dir == 1)fillStep(i, color, true);
+      else fillStep(i, mRGB(0, 0, 0), false);
+    }
+  }
+}
+// ========= залить ступеньку цветом (служебное)
+void fillStep(int8_t num, LEDdata color, bool mode) {
+  if (num >= STEP_AMOUNT || num < 0) return;
+  for (int i = steps_start[num]; i < steps_start[num] + steps[num].led_amount; i++) {
+    if (mode) {
       stripLEDs[i] = color;
     }
+    else {
+      stripLEDs[i] = mRGB(0, 0, 0);
+    }
+  }
+  Serial.println("Fade");
+  fadeStep(num, mode);
+  tmr = millis();
+  while (millis() - tmr < FADR_SPEED) {};
+}
+
+void fillStepWithBitMask(int8_t num, LEDdata color, uint32_t bitMask) {
+  if (num >= STEP_AMOUNT || num < 0) return;
+  for (int i = steps_start[num]; i < steps_start[num] + steps[num].led_amount; i++) {
+    if (bitRead(bitMask, (i - steps_start[num]) % 16)) {
+      stripLEDs[i] = color;
+    }
+  }
+}
+
+void smooth(int bright) {
+  int changeBright = 0;
+  do {
+    delay(SMOOTH_DELAY);
+    setBrightness(changeBright);
     show();
-  }
+    changeBright += 5;
+  } while (changeBright < bright);
+}
 
-  void fillStepWithBitMask(int8_t num, LEDdata color, uint32_t bitMask) {
-    if (num >= STEP_AMOUNT || num < 0) return;
-    for (int i = steps_start[num]; i < steps_start[num] + steps[num].led_amount; i++) {
-      if (bitRead(bitMask, (i - steps_start[num]) % 16)) {
-        stripLEDs[i] = color;
-      }
-    }
-  }
+void setBrightness(int brightness) {
+  strip.setBrightness(brightness);
+}
 
-  void animatedSwitchOff(int bright) {
-    int changeBright = bright;
-    while (changeBright > 0) {
-      delay(50);
-      setBrightness(changeBright);
-      show();
-      changeBright -= 5;
-    }
-  }
+void show() {
+  strip.show();
+}
 
-  void animatedSwitchOn(int bright) {
-    int changeBright = 0;
-    do {
-      delay(50);
-      setBrightness(changeBright);
-      show();
-      changeBright += 5;
-    } while (changeBright < bright);
-  }
-
-  void setBrightness(int brightness) {
-    strip.setBrightness(brightness);
-  }
-
-  void show() {
-    strip.show();
-  }
-
-  void clear() {
-    strip.clear();
-  }
+void clear() {
+  strip.clear();
+}
